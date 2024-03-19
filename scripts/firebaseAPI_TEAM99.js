92fd5b2bdc483f48771f117e689ea99fa20dcb57
Jordan Nykoluk <jnykoluk22@gmail.com>
2024-03-19 09:09:24 -0700

    updated to demo 09

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,13 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+const firebaseConfig = {
+  apiKey: "AIzaSyCHl8dKpn89Uj6ljOZhdMh1i9oMrom4IDY",
+  authDomain: "comp1800-202410-demo-1a4f7.firebaseapp.com",
+  projectId: "comp1800-202410-demo-1a4f7",
+  storageBucket: "comp1800-202410-demo-1a4f7.appspot.com",
+  messagingSenderId: "403178287590",
+  appId: "1:403178287590:web:04accca5ded9df4ca86627",
+  measurementId: "G-352JFMVCZ9"
 };
 
 //--------------------------------------------
